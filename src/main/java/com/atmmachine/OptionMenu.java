fcfeb66cfd9ed7d39965b053e7f7f7145e22dd67
Bruno Gusmão <brunogusmao43@gmail.com>
2023-02-03 19:42:13 -0300

    getting the user's card data into a dicitionary with the map interface

--- src/main/java/com/atmmachine/OptionMenu.java
+++ src/main/java/com/atmmachine/OptionMenu.java
@@ -2,7 +2,11 @@ package com.atmmachine;
 
 import java.text.DecimalFormat;
 import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
+import java.util.Map.Entry;
+
+import javax.swing.text.html.HTMLDocument.Iterator;
 
 public class OptionMenu {
     Scanner menuInput = new Scanner(System.in);
@@ -20,13 +24,27 @@ public class OptionMenu {
                 cardnumber = menuInput.nextInt();
                 System.out.println("\n Enter your PIN: ");
                 pinnumber = menuInput.nextInt();
-                data.entrySet().iterator();
+                java.util.Iterator<Entry<Integer, Account>> iterator = data.entrySet().iterator();
 
+                do {
+                    Map.Entry key = (Map.Entry) iterator.next();
+                    Account account = (Account) key.getValue();
+                    if (data.containsKey(cardnumber) && pinnumber == getAccountType(account));
+                    break;
 
-            } catch (Exception e) {
-                
+                } while (iterator.hasNext());
+
+            }
+            catch (Exception e) {
+                System.out.println("\n Invalid Character(s). Please only add numbers.");
+            } finally {
+                auth = false;
             }
         }
 
+    }
+    //TODO:Make the function getAccountType next.
+    private int getAccountType(Account account) {
+        return 0;
     }
 }
