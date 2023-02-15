551e36c1de6286f60cd3f58a68c7a372d2cdcff1
Bruno Gusmão <brunogusmao43@gmail.com>
2023-02-15 15:20:22 -0300

    implementing the methods on Account

--- src/main/java/com/atmmachine/Account.java
+++ src/main/java/com/atmmachine/Account.java
@@ -1,8 +1,28 @@
 package com.atmmachine;
 
+import java.text.DecimalFormat;
+import java.util.Scanner;
+
 public class Account {
 
-    public Account(int no, int pinnumber) {
+    private int customerNumber;
+    private int pinNumber;
+    private double checkingBalance = 0;
+	private double savingBalance = 0;
+
+    Scanner in = new Scanner(System.in);
+    DecimalFormat money = new DecimalFormat("'$'###,##0.00");
+
+    public Account(int no, int pinnumber, int customerNumber) {
+        this.customerNumber = customerNumber;
+        this.pinNumber = pinnumber;
+    }
+
+    public Account(int customerNumber, int pinNumber, double checkingBalance, double savingBalance) {
+        this.customerNumber = customerNumber;
+        this.pinNumber = pinNumber;
+        this.checkingBalance = checkingBalance;
+        this.savingBalance = savingBalance;
     }
 
     public void getSavingWithdrawInput() {
